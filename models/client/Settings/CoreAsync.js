830c64a983fcb43a70fa2407c1893937c2580a81
Carlos Vieira <carlosvs1996@gmail.com>
2021-08-13 18:31:49 +0200

    bug fix

--- models/client/Settings/CoreAsync.js
+++ models/client/Settings/CoreAsync.js
@@ -2,7 +2,7 @@ import { icons } from "./_dependencies"
 
 export async function CoreAsync() {
 	return {
-		hostname: process.env.NEXT_PUBLIC_NEXT_PUBLIC_HOSTNAME,
+		hostname: process.env.NEXT_PUBLIC_HOSTNAME,
 		port: process.env.NEXT_PUBLIC_PORT,
 		host: process.env.NEXT_PUBLIC_HOST,
 		domainUrl: process.env.NEXT_PUBLIC_DOMAIN_URL,
