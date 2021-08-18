8f65e962dd8a16a9a342636f619d83bab1fbde4e
Carlos Vieira <carlosvs1996@gmail.com>
2021-08-18 14:07:27 +0200

    remove redundant error

--- models/server/Token/encryptAsync.js
+++ models/server/Token/encryptAsync.js
@@ -4,10 +4,5 @@ export async function encryptAsync({ token }) {
 	const [tokenValue, errorWithTokenValue] = Try(() => token[token.type])
 	if (errorWithTokenValue) throw Error("Unable to read token value.")
 
-	const [encryptedTokenValue, errorWithEncryptedTokenValue] = await Try(() =>
-		Crypto.encryptAsync(tokenValue),
-	)
-	if (errorWithEncryptedTokenValue) throw Error("Unable to encrypt token.")
-
-	return { ...token, [token.type]: encryptedTokenValue, encrypted: true }
+	return { ...token, [token.type]: Crypto.encryptAsync(tokenValue), encrypted: true }
 }
