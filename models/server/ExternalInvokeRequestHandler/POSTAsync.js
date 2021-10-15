76aad3aeba7da114c50c038321f3875d26f38748
Carlos <codebycarlos@outlook.com>
2021-10-15 23:44:39 +0100

    disabled external invocation for demo

--- models/server/ExternalInvokeRequestHandler/POSTAsync.js
+++ models/server/ExternalInvokeRequestHandler/POSTAsync.js
@@ -1,30 +1,12 @@
-import { API, APIKeyPair, JSend, Try } from "./_dependencies"
+import { JSend, Try } from "./_dependencies"
 export async function POSTAsync(req, res) {
 	const { ScriptsAPI } = await API.loadScriptsAPIAsync()
 
 	if (req.query?.auth_type !== "apikey" || !req.query?.apikey)
 		return JSend(res).error({ message: "Unauthorised access." }, 401)
 
-	const [accessGranted] = await Try(() =>
-		APIKeyPair.confirmKeyMatchAsync({
-			FunctionName: req.query.FunctionName,
-			Qualifier: req.query?.Qualifier,
-			apikey: req.query.apikey,
-		}),
+	return JSend(res).error(
+		{ message: "Sorry, external script invocation is disabled for this demo." },
+		401,
 	)
-
-	if (accessGranted !== true) return JSend(res).error({ message: "Unauthorised access." }, 401)
-
-	const [response, error] = await Try(() =>
-		ScriptsAPI.invokeScriptAsync({
-			...req.query,
-			...req?.body,
-			InvocationType:
-				req.query?.invocation_type === "asynchronous" ? "Event" : "RequestResponse",
-		}),
-	)
-
-	if (error) return JSend(res).error({ message: error?.message })
-
-	return JSend(res).success({ data: { response } })
 }
--- models/server/ExternalInvokeRequestHandler/_dependencies.js
+++ models/server/ExternalInvokeRequestHandler/_dependencies.js
@@ -1,5 +1,3 @@
 export { RequestHandler } from "models/server/RequestHandler"
-export { API } from "models/server/API"
 export { JSend } from "indigozest_modules/models/server/JSend"
 export { Try } from "indigozest_modules/models/client/Try"
-export { APIKeyPair } from "models/server/APIKeyPair"
