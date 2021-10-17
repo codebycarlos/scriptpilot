12ae257a4b21c7b09fcca55c5c5ca84e66064768
Carlos <codebycarlos@outlook.com>
2021-10-17 20:03:40 +0100

    brand message change

--- components/pages/LoginPage/propsDefinition.js
+++ components/pages/LoginPage/propsDefinition.js
@@ -10,7 +10,7 @@ export function propsDefinition(PropTypes) {
 		},
 		welcomeText: {
 			propType: PropTypes.string.isRequired,
-			defaultProp: "CLOUD POWERED SCRIPTS",
+			defaultProp: "CLOUD POWERED COMPUTING",
 		},
 	}
 }
