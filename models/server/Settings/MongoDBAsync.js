c907ddb1d18646ac47d44d3b27c60e74098fea0f
Carlos <codebycarlos@outlook.com>
2021-10-18 16:02:32 +0100

    Removed unnecessary properties

--- models/server/Settings/MongoDBAsync.js
+++ models/server/Settings/MongoDBAsync.js
@@ -1,8 +1,5 @@
 export async function MongoDBAsync() {
 	return {
-		cluster0UserName: process.env.MONGODB_CLUSTER0_USERNAME,
-		cluster0Password: process.env.MONGODB_CLUSTER0_PASSWORD,
-		defaultDatabseName: process.env.MONGODB_CONNECT_DATABASE_NAME,
 		connectUrl: process.env.MONGODB_CONNECT_URL,
 		encryptionAlgorithm: process.env.MONGODB_ENCRYPTION_ALGORITHM,
 		encryptionSecretKey: process.env.MONGODB_ENCRYPTION_SECRET_KEY,
