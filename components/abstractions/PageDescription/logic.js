9beeeb5f8b1549f0bbf63897121fb7c327f80f01
Carlos Vieira <codebycarlos@outlook.com>
2021-12-09 20:26:40 +0000

    Update logic.js

--- components/abstractions/PageDescription/logic.js
+++ components/abstractions/PageDescription/logic.js
@@ -2,8 +2,8 @@ export function logic(imports, props, styleDefault) {
 	if (props.page === "scripts") {
 		props.descriptionText = (
 			<>
-				A &nbsp; script &nbsp;is code which executes a set of instructions. It may be&nbsp;
-				triggered &nbsp;manually, or externally via a HTTP request (disabled for this demo).
+				A script is code which executes a set of instructions. It may be 
+				triggered manually, or externally via a HTTP request (disabled for this demo).
 			</>
 		)
 	}
